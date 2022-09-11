d9a4b2cbcfebde0e2cdbe1b1cb0769680d0855c0
mcmin001 <mcmin001@sina.com>
2022-09-12 00:19:30 +0800

    提交代码

--- hello-springboot/springboot-web/src/main/java/com/hello/springboot/web/WebAppServer.java
+++ hello-springboot/springboot-web/src/main/java/com/hello/springboot/web/WebAppServer.java
@@ -1,4 +1,13 @@
 package com.hello.springboot.web;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.hello.springboot")
+@EntityScan("com.hello.springboot.model")
+@Slf4j
 public class WebAppServer {
 }
